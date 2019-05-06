f867ba1fb5ba83ec5daae765148e2140dcf3b6ad
Ashi Thilakarathna <Ashi.Thilakarathna@myob.com>
2019-05-06 12:18:31 +1000

    Test 02 - Adding second line

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -8,6 +8,7 @@ namespace ConsoleApp1
         {
             Console.WriteLine("Hello World!");
             Console.WriteLine("Hello World from Ashi!");
+            Console.WriteLine("Second Line!");
         }
     }
 }
